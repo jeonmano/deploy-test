5060997175c58b664845cfc281613ae8500aba54
jeonmano <gochujang87@gmail.com>
2024-03-03 16:18:02 +0900

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
+const os = require('os');
 
 const app = express();
 
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.send('hello root');
+  res.send(`hello root ${os.hostname}`);
+});
+app.get('/app', (req, res) => {
+  res.send(`hello app ${os.hostname}`);
+});
+app.get('/app/a', (req, res) => {
+  res.send(`hello app  a ${os.hostname}`);
+});
+app.get('/asdf', (req, res) => {
+  res.send(`hello asdf ${os.hostname}`);
+});
+
+app.get('/asdf/a', (req, res) => {
+  res.send(`hello asdf  a ${os.hostname}`);
 });
 
 app.listen(3000, () => {
